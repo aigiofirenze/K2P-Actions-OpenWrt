29c61f09f0e703aa0e05b39a7ecc3d8b19fa3b30
aigiofirenze <50103096+aigiofirenze@users.noreply.github.com>
2020-10-13 10:52:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,6 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+#git clone https://github.com/vernesong/luci-app-clash package/molun/luci-app-clash
+#git clone https://github.com/fw876/helloworld package/molun/luci-app-ssr-plus
