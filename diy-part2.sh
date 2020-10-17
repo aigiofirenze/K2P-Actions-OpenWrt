2d6f0847c85f1da5cb393ddf5da69e1bd934d13c
aigiofirenze <50103096+aigiofirenze@users.noreply.github.com>
2020-10-17 14:36:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,7 +8,6 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#引用liuran001软件包源码
-git clone https://github.com/liuran001/openwrt-packages package
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
