4ab300cea8ac0f1119819891856cf19b08eae977
aigiofirenze <50103096+aigiofirenze@users.noreply.github.com>
2021-10-31 14:01:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,9 +8,10 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+  git clone https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-echo '添加SSRPLUS软件源'
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-cat feeds.conf.default
-echo '=========Add feed source OK!========='
+# Add a feed source
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
