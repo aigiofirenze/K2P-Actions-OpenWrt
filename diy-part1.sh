e9ebcd6ba01557fe64702741a667e5bbd6a2fc12
aigiofirenze <50103096+aigiofirenze@users.noreply.github.com>
2021-10-31 13:20:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,12 +10,7 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+echo '添加SSRPLUS软件源'
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-
+cat feeds.conf.default
+echo '=========Add feed source OK!========='
